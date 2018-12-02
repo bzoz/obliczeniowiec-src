31ca90212d992a56e76e8ce35f3fb1240665ddbc
Bartosz Sosnowski <bartosz@janeasystems.com>
2018-12-02 20:35:24 +0100

    Updates

---
+++ precache-manifest.d75b0478b32e58a7368d0a99aab3de54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "664cd33e378dcf79bb1a",
+    "url": "/obliczeniowiec/static/js/runtime~main.664cd33e.js"
+  },
+  {
+    "revision": "6eb0f590c8d3e04f082c",
+    "url": "/obliczeniowiec/static/js/main.6eb0f590.chunk.js"
+  },
+  {
+    "revision": "7cd2f7f06303e1df5a9e",
+    "url": "/obliczeniowiec/static/js/1.7cd2f7f0.chunk.js"
+  },
+  {
+    "revision": "6eb0f590c8d3e04f082c",
+    "url": "/obliczeniowiec/static/css/main.64eddd32.chunk.css"
+  },
+  {
+    "revision": "7cd2f7f06303e1df5a9e",
+    "url": "/obliczeniowiec/static/css/1.4ff660ee.chunk.css"
+  },
+  {
+    "revision": "c054ff98d35ec212413786c688b218ca",
+    "url": "/obliczeniowiec/index.html"
+  }
+];
